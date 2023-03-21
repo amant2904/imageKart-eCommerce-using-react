0d0d12b8102e1a763ec37632e44ac7217d5636d2
amant2904 <tiwariaman2904@gmail.com>
2023-03-21 19:37:47 +0530

    added form for adding movies manually

--- src/Components/Pages/Movies.js
+++ src/Components/Pages/Movies.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react'
-import { Container, Button, Row } from 'react-bootstrap'
+import { Container, Button, Row, Form } from 'react-bootstrap'
 import classes from "./Movies.module.css"
 import loader from "../../Assets/loader.png"
 
@@ -7,6 +7,9 @@ export default function Movies() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [movieName, setMovieName] = useState("");
+    const [movie_openingText, setMovieOpeningText] = useState("");
+    const [movieDirector, setMovieDirector] = useState("");
 
     const api_url = "https://swapi.dev/api/films";
     const fetchAPI_handler = useCallback(async () => {
@@ -67,8 +70,51 @@ export default function Movies() {
         </Container>
     }
 
+    // manually adding movie form 
+
+    const movieName_handler = useCallback((e) => {
+        setMovieName(e.target.value);
+    }, [])
+
+    const movie_openingText_handler = useCallback((e) => {
+        setMovieOpeningText(e.target.value);
+    }, [])
+
+    const movieDirector_handler = useCallback((e) => {
+        setMovieDirector(e.target.value);
+    }, [])
+
+    const movieSubmit_handler = useCallback((e) => {
+        e.preventDefault();
+        let newMovieObj = {
+            name: movieName,
+            director: movieDirector,
+            opening_text: movie_openingText
+        }
+        console.log(newMovieObj);
+    }, [movieName, movieDirector, movie_openingText])
+
     return (
         <Container className={`${classes.mainCont}`}>
+            <Container className={`${classes.formCont} p-4 my-4`}>
+                <Form onSubmit={movieSubmit_handler}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Movie Name" value={movieName} onChange={movieName_handler} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Opening Text</Form.Label>
+                        <Form.Control as="textarea" rows={3} placeholder="Enter Opening Text Here" value={movie_openingText} onChange={movie_openingText_handler} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Director Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Director Name" value={movieDirector} onChange={movieDirector_handler} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className={classes.formSubmit_btn}>
+                        Set Movie
+                    </Button>
+                </Form>
+            </Container>
             <Container className={`${classes.btnCont} p-4 my-4 d-flex align-items-center justify-content-center`}>
                 <Button className={`${classes.fetchBtn}`} onClick={fetchAPI_handler}>Fetch Movies</Button>
             </Container>
